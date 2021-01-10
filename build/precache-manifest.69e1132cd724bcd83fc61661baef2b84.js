319ede2a32cc3f052669381889806b4b436b1a58
Rafaa17 <andreou_rafael@hotmail.com>
2021-01-10 13:25:47 +0200

    Updates

---
+++ build/precache-manifest.69e1132cd724bcd83fc61661baef2b84.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70511854cd6bd152f24735c873c2afea",
+    "url": "/Rafaa17/react-custom-modal/index.html"
+  },
+  {
+    "revision": "66e17081c594e14e1255",
+    "url": "/Rafaa17/react-custom-modal/static/css/2.e6ac38db.chunk.css"
+  },
+  {
+    "revision": "74c5d7b1b4809f37ec59",
+    "url": "/Rafaa17/react-custom-modal/static/css/main.a0a82bbe.chunk.css"
+  },
+  {
+    "revision": "66e17081c594e14e1255",
+    "url": "/Rafaa17/react-custom-modal/static/js/2.c74f25be.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Rafaa17/react-custom-modal/static/js/2.c74f25be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74c5d7b1b4809f37ec59",
+    "url": "/Rafaa17/react-custom-modal/static/js/main.f4899080.chunk.js"
+  },
+  {
+    "revision": "6b7e3717b9e9649fbdc5",
+    "url": "/Rafaa17/react-custom-modal/static/js/runtime-main.39ad6b84.js"
+  }
+]);
