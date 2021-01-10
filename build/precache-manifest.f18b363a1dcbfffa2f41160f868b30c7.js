e376f7ef5cb09abd35fe77babe6c175f5ecd62fe
Rafaa17 <andreou_rafael@hotmail.com>
2021-01-10 20:37:50 +0200

    Updates

---
+++ build/precache-manifest.f18b363a1dcbfffa2f41160f868b30c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cbfc588f3a44eb7c89cee9b16fb0602",
+    "url": "/react-custom-modal/index.html"
+  },
+  {
+    "revision": "66e17081c594e14e1255",
+    "url": "/react-custom-modal/static/css/2.e6ac38db.chunk.css"
+  },
+  {
+    "revision": "118560897fd7395c634a",
+    "url": "/react-custom-modal/static/css/main.db3307fa.chunk.css"
+  },
+  {
+    "revision": "66e17081c594e14e1255",
+    "url": "/react-custom-modal/static/js/2.c74f25be.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-custom-modal/static/js/2.c74f25be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "118560897fd7395c634a",
+    "url": "/react-custom-modal/static/js/main.205e2d3a.chunk.js"
+  },
+  {
+    "revision": "0f7e3a27add16c2e16fe",
+    "url": "/react-custom-modal/static/js/runtime-main.41bd4a6b.js"
+  }
+]);
